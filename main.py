4cc2465f43e43ee222b69a0d92884365fd7298bf
Goci13 <139585961+Goci13@users.noreply.github.com>
2026-01-26 17:55:10 +0100

    fix

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ if selected == 'Home':
             """
         )
     with spalte2:
-        st.image('assets/python.jpg')
+        st.image('assets/Python Robotik.png')
     st.write('---')
 
 ########################## Dritte Zeile #########################
@@ -174,6 +174,7 @@ if selected == 'Kontakt':
     )
 
     st.write("<p style='color: #00FFC4;'>mikic-clan@freenet.de</p>", unsafe_allow_html=True)
+
 
 
 
